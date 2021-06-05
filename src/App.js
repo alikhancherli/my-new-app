050e59bfb5913e0378ff7b03546a285af9708205
AliKhancherli <a.khancherli2017@outlook.com>
2021-06-05 16:29:42 +0430

    first commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Button, Col, Container, Row} from 'react-bootstrap';
+function App() {
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <Button block>Btn 1</Button>
+        </Col>
+
+        <Col>
+        <Button block variant='success'>Btn 1</Button>
+        </Col>
+
+        <Col>
+          <p>
+            Salam
+          </p>
+        </Col>
+
+        <Col>
+          <p>
+            Salam
+          </p>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default App;
